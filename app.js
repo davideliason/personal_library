a95b259d919294dc59423cac126c963cf0b86a4e
David Eliason <davethemaker@gmail.com>
2018-08-27 17:37:31 -0700

    combined server.js and database.js code and functionality within single script to first establish connection to the database and once that successfully happened to serve a template view of db query

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express"),
+    app     = express(),
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+MongoClient.connect('mongodb://localhost:27017/personal_library', function(err, db) {
+
+    assert.equal(err, null);
+    console.log("Successfully connected to MongoDB.");
+
+    app.get('/', (req,res)=>{
+        db.collection('personal_library').find({}).toArray(function(err, docs) {
+
+            res.render('book', { 'books' : docs});
+           
+         });
+    });
+
+  
+
+    app.listen(8080, () => {
+        console.log("listening at port 8080");
+    });
+
+});
+
+
+

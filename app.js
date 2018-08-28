c698de5eee8775975d8db7760a71b2058298a0b9
David Eliason <davethemaker@gmail.com>
2018-08-27 18:10:02 -0700

    pass in array of hard-coded title values as parameter for rendered view on home page

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require("express"),
     app     = express(),
     engines = require('consolidate'),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
+    assert = require('assert'),
+    bodyParser = require('body-parser');
 
+// middleware
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({extended: true}));
 
 MongoClient.connect('mongodb://localhost:27017/personal_library', function(err, db) {
 
@@ -14,7 +17,7 @@ MongoClient.connect('mongodb://localhost:27017/personal_library', function(err, 
     console.log("Successfully connected to MongoDB.");
 
     app.get('/', (req,res)=>{
-        res.render('home', {'user' : "David"});
+        res.render('home', {'user' : "David", "titles" : ['2001', 'Dune']});
     });
 
     app.get("/books", (req,res) =>{

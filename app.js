3f1e83e029023bd6454f6725440f35f052c91022
David Eliason <davethemaker@gmail.com>
2018-08-31 12:37:38 -0700

    set template engine and simple render page. established link to mongodb and retrieved the single data there and rendered to page

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var logger = require('morgan');
+var MongoClient = require('mongodb').MongoClient;
+var engines = require('consolidate');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+MongoClient.connect('mongodb://localhost:27017/', function(err,db){
+    var dbo = db.db('personal_library');
+
+    app.get('/', (req,res) => {
+        dbo.collection('mybooks').find().toArray((err,docs)=>{
+            console.log(docs);
+        });
+
+    });
+});
+
+app.listen(8080, ()=>{
+    console.log("listening at 8080");
+});

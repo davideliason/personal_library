20b034c5fe6a7ffed1c27dd0453c8c3d0bafb07f
David Eliason <davethemaker@gmail.com>
2018-08-31 13:18:56 -0700

    successfully create new POST route directing to form with two text inputs, saving POSTED data object to DB, redirecting to root route, and rendering all saved data objects including the recently added one

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var logger = require('morgan');
 var MongoClient = require('mongodb').MongoClient;
 var engines = require('consolidate');
+var bodyParser = require('body-parser');
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: false }));
 
 MongoClient.connect('mongodb://localhost:27017/', function(err,db){
     var dbo = db.db('personal_library');
@@ -19,6 +22,27 @@ MongoClient.connect('mongodb://localhost:27017/', function(err,db){
             
         });
 
+    });
+
+    app.get('/newbook',(req,res)=>{
+        res.render('newbook', { "title" : "Add new book"});
+    });
+
+    app.post('/addbook', (req,res)=>{
+        var newTitle = req.body.newtitle;
+        var newAuthor = req.body.newauthor;
+
+        console.log("okay posted" + newTitle + " " + newAuthor);
+
+        dbo.collection('mybooks').insertOne({
+            "title" : newTitle,
+            "author" : newAuthor
+        },(err,doc)=>{
+            if(err){res.send("error saving info")}
+            else {
+                res.redirect("/");
+            }
+        });
     });
 });
 
